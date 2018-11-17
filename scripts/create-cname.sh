fc55071fae66813ecde885f24d28ce5a41e7d67e
Or Fleisher <fleisher.or@gmail.com>
2018-11-17 17:46:46 -0500

    Fixing the CNAME script right in the yml file itself

--- scripts/create-cname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Tiny util to generate CNAME files with the right domain
-
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-echo "Working directory is $parent_path"
-touch "$parent_path/public/CNAME"
-echo 'orfleisher.com' > "$parent_path/public/CNAME"
-echo "Created CNAME file"
