4cf9923185cac9cc87994ee80506ba9dcf5477c6
Or Fleisher <fleisher.or@gmail.com>
2018-11-17 17:32:42 -0500

    Testing a new 404 page

--- scripts/create-cname.sh
+++ scripts/create-cname.sh
@@ -4,6 +4,6 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 echo "Working directory is $parent_path"
-touch "public/CNAME"
-echo 'orfleisher.com' > "public/CNAME"
+touch "$parent_path/public/CNAME"
+echo 'orfleisher.com' > "$parent_path/public/CNAME"
 echo "Created CNAME file"
---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+export default class NotFoundPage extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <div>Oops, page not found!</div>
+    )
+  }
+
+}
